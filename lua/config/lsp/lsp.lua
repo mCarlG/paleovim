352c456729f58f7cff93e865617e7ea6f95f12a6
mCarlG <mcg1441@uncw.edu>
2023-04-10 13:24:33 -0400

    Added functionality to Mason-LSP to automagically configure language servers.

--- lua/config/lsp/lsp.lua
+++ lua/config/lsp/lsp.lua
@@ -2,30 +2,6 @@ local nvim_lsp = require "lspconfig"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
 }
-
-local servers = {
-  "bashls",
-  "dockerls",
-  "jsonls",
-  "pyright",
-  "sumneko_lua",
-  "terraformls",
-  "texlab",
-  "tsserver",
-  "yamlls",
-  "rust_analyzer",
-}
-
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    before_init = function(_, config)
-      if lsp == "pyright" then config.settings.python.pythonPath = utils.get_python_path(config.root_dir) end
-    end,
-    capabilities = capabilities,
-    flags = { debounce_text_changes = 150 },
-    settings = {},
-  }
-end
--- lua/options.lua
+++ lua/options.lua
@@ -9,5 +9,5 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.showmode = false
 vim.opt.scrolloff = 8
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
