90e4d35fb6f69e1109848972382ca91956bae8fa
Michael Gogarty <mgogarty@researchinnovations.com>
2024-10-14 16:08:17 -0400

    Add file and string search, file trees, and key mappings.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,17 @@ local function map(mode, lhs, rhs, opts)
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- For leader
 map('n', " ", "<NOP>")
-map('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 zsh <CR> ")
+
+-- Terminal
+map('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 zsh<CR>")
 map('n', "t", ":FloatermToggle myfloat<CR>")
 map('t', "<Esc>", "<C-\\><C-n>:q<CR>")
+
+-- Telescope
+map('n', "<leader>o", ":Telescope find_files<CR>")
+map('n', "<leader>f", ":Telescope live_grep<CR>")
+
+-- Nvim-Tree
+map('n', "<leader>td", ":NvimTreeToggle<CR>")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -122,4 +122,40 @@ require("lazy").setup({
 			}
 		end
 	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup {
+				filters = {
+					dotfiles = true,
+				},
+			}
+		end
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup {
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			}
+			require("telescope").load_extension("fzf")
+		end
+	},
 })
